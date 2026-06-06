b2c8dca90b018bd05f78c13bbe2dd6b2e46de843
ThrilledBaboon <johnsonbalmerj.17@gmail.com>
2026-06-06 18:01:27 +0100

    init commit with template

---
+++ src/main/java/slimeygrandiose/consecutivechallenge/ConsecutiveChallengeDataGenerator.java
@@ -0,0 +1,11 @@
+package slimeygrandiose.consecutivechallenge;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ConsecutiveChallengeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
